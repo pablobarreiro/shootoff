7ef8b03465e765a7bc2549356a56bee3129522e9
FrannDemon <franndemontis@gmail.com>
2022-06-07 15:51:00 -0300

    primera rutas del usuario

---
+++ shoot-off/api/routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express") ;
+const userRouter = express.Router() ;
+const { User } = require("../models")
+const passport = require("passport")
+
+
+userRouter.get("/register", (req, res) => {
+    User.create(req.body).then((user) => res.status(201).send(user))
+});
+
+userRouter.post("/login", passport.authenticate("local"), (req, res) => {
+    res.send(req.user)
+});
+
+userRouter.post("/logout", (req, res) => {
+    req.logOut();
+    res.sendStatus(200);
+})
+
+userRouter.get("/me", (req, res) => {
+    if(!req.user){
+        return res.sendStatus(401);
+    }
+})
+
+
+
+
+
+module.exports = userRouter ;
