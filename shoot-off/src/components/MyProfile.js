40c34b1c512460343cd71aaece201d4eb2289434
FrannDemon <franndemontis@gmail.com>
2022-06-15 18:06:54 -0300

    boton en perfil admin

--- shoot-off/src/components/MyProfile.js
+++ shoot-off/src/components/MyProfile.js
@@ -4,6 +4,7 @@ import useInput from "../commons/useInput";
 import { AuthContext } from "../context/GlobalState";
 import { ReqContext } from "../context/RequestState";
 import swal from "sweetalert";
+import { Link } from "react-router-dom"
 
 const Input = ({state,onChange}) => {
     return (
@@ -146,7 +147,13 @@ const MyProfile = () => {
                 <p className="checkout">Edit</p>
             </button>
         </div>}
+            <div className="mb-3">
+                {user.admin ? <Link to={"/users/admin"} className="btn btn-dark  "> 
+                    <p className="checkout">User list</p>
+                </Link> : <></>}
+            </div>
       </div>
+       
     </>
   );
 };
--- shoot-off/src/components/UserAdmin.js
+++ shoot-off/src/components/UserAdmin.js
@@ -26,10 +26,12 @@ const UserAdmin = () => {
 
   const handleDeleteClick = (contactId) => {
     const newContacts = [...contacts];
-
     const index = contacts.findIndex((contact) => contact.id === contactId);
+    
     newContacts.splice(index, 1);
     setContacts(newContacts);
+    axios.delete(`/api/user/admin/1/deleteUser/${contactId}`)
+    
   };
 
   return (
