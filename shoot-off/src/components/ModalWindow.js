8246f196a2d4d9ca8708dfa76a9178f536070046
FrannDemon <franndemontis@gmail.com>
2022-06-16 11:33:43 -0300

    admin/employee funciones adicionales

--- shoot-off/src/components/ModalWindow.js
+++ shoot-off/src/components/ModalWindow.js
@@ -8,61 +8,67 @@ import { useNavigate } from "react-router-dom";
 
 export const ModalWindow = () => {
   const [stateModal, setStateModal] = useState(false);
-   const navigate=useNavigate()
-    const product_name= useInput("")
-    const description= useInput("")
-    const price = useInput("")
-    const category= useInput("")
-    const stock= useInput("")
-    const img_url= useInput("")
+  const navigate = useNavigate();
+  const product_name = useInput("");
+  const description = useInput("");
+  const price = useInput("");
+  const category = useInput("");
+  const stock = useInput("");
+  const img_url = useInput("");
 
   const openCloseModal = () => {
     setStateModal(!stateModal);
   };
 
-  const handleModel=(e)=>{
-      e.preventDefault();
-      axios.post("/api/product",{
+  const handleModel = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/product", {
         product_name: product_name.state,
-        description:description.state,
-        price:Number(price.state),
-        category:category.state,
-        stock:Number(stock.state),
-        img_url:img_url.state
+        description: description.state,
+        price: Number(price.state),
+        category: category.state,
+        stock: Number(stock.state),
+        img_url: img_url.state,
       })
-      .then(res=>res.data)
-      .then(()=>{
-          swal({tittle:"Created", text:"Product created", icon:"success"})
-          navigate("/")
-      })
-  }
-
-  
+      .then((res) => res.data)
+      .then(() => {
+        swal({ tittle: "Created", text: "Product created", icon: "success" });
+        navigate("/");
+      });
+  };
 
   return (
     <>
-      <Button className="container" onClick={() => openCloseModal()}>
+      <button className="btn btn-dark" onClick={() => openCloseModal()}>
         Add product
-      </Button>
+      </button>
       <Modal open={stateModal} onClose={openCloseModal}>
-
         <form className="modal-product" onSubmit={handleModel}>
           <div>
             <h2>Add product</h2>
           </div>
 
-          <TextField label="product_name" className="textField" {...product_name}/>
-      <br />
-      <TextField label="description" className="textField" {...description}/>
-      <br />
-      <TextField label="price" className="textField" {...price}/>
-      <br />
-      <TextField label="category" className="textField"{...category} />
-      <br />
-      <TextField label="stock" className="textField" {...stock}/>
-      <br />
-      <TextField label="img_url" className="textField" {...img_url} />
-      <br />
+          <TextField
+            label="product_name"
+            className="textField"
+            {...product_name}
+          />
+          <br />
+          <TextField
+            label="description"
+            className="textField"
+            {...description}
+          />
+          <br />
+          <TextField label="price" className="textField" {...price} />
+          <br />
+          <TextField label="category" className="textField" {...category} />
+          <br />
+          <TextField label="stock" className="textField" {...stock} />
+          <br />
+          <TextField label="img_url" className="textField" {...img_url} />
+          <br />
           <div>
             <Button type="submit">Add Product</Button>
             <Button onClick={() => openCloseModal()}>close</Button>
